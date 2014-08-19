d1862ee09011d42658b59733ab7310b612b70a67
Naph <lachlandupreez@gmail.com>
2014-08-20 02:25:01 +1000

    Added dummy page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Will Code Review</title>
+</head>
+
+<body>
+<h1>It Works!</h1>
+</body>
+</html>
