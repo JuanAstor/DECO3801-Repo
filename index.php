38729026da59dcd9d2f3aa65156ce6f4ef739545
Naph <lachlandupreez@gmail.com>
2014-08-21 23:48:11 +1000

    Added CSS File

--- index.php
+++ index.php
@@ -6,11 +6,18 @@ include("resources/db.php")
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="/public_html/css/main.css">
+<script src="/public_html/js/jquery.min.2.1.1.js"></script>
+<script src="/public_html/js/view.js"></script>
 <title>Will Code Review</title>
 </head>
 
 <body>
-<h1>It Works!</h1>
-<?php echo @mysql_ping() ? 'true' : 'false'; ?>
+    <header>
+    <h1>It Works!</h1>
+    </header>
+    <footer>
+    <?php echo @mysql_ping()?'':'Database Not Connected';?>
+    </footer>
 </body>
 </html>
