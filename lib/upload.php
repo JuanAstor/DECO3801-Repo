69c4a9a1b601e3322690ffac7b133eec3ffefd94
Steve Jacobson <steven.jacobson@uqconnect.edu.au>
2014-09-03 18:54:00 +1000

    now checks that a file is being uploaded and is of the correct file extension. Also added comments on the 'upload.php' file

--- lib/upload.php
+++ lib/upload.php
@@ -2,28 +2,46 @@
 <?php //upload a file to the database
 	require("mysql.php");
 	require("queries.php");
-	if($_FILES["userfile"]["type"] != "file/txt" && $_FILES["userfile"]["size"] < 0 ){
-		echo "Error: ". $_FILES["userfile"]["error"]. "<br>";
-	} else {
-		$fileName = $_FILES['userfile']['name'];
-		$tmpName = $_FILES['userfile']['tmp_name'];
-		$fileSize = $_FILES['userfile']['size'];
-		$fileType = $_FILES['userfile']['type'];
-		
-		$fp = fopen($tmpName, 'r');
-		$content = fread($fp, filesize($tmpName));
-		$content = mysql_real_escape_string($content); 
-		fclose($fp); 
-		
-		$fileName = str_replace(' ', '', $fileName); 
-		$fileName = pathinfo($fileName, PATHINFO_FILENAME);
-		
-		$fileName = addslashes($fileName); 
-		
-		$query = "INSERT INTO `assignmentfile`(`AssignmentID`, `UserID`, `FileID`, `FileName`, `FileData`) VALUES 
-		('1','Bob','1','$fileName','$content')";
-		
-		MySQL::getInstance()->query($query) or die('<br>Error, file upload failed '. mysql_error());
-		echo "<br>File <i>$fileName</i> uploaded!</br>";
+	$allowedExts = array("txt", "php", "html"); //the array holding all the allowed file extensions
+	$temp = explode(".", $_FILES["userfile"]["name"]); //strip at '.'
+	$extension = end($temp); //get the extension of the file
+	
+	//check if the uploaded file has size greater than 0 and is an allowed extension
+	if($_FILES["userfile"]["size"] > 0 && in_array($extension, $allowedExts) ){
+		if($_FILES["userfile"]["error"] > 0) { //check if any errors in the file
+			echo "Error: ". $_FILES["userfile"]["error"]. "<br>";
+			echo "Something happened here";
+		}
+		else { //no errors
+			$fileName = $_FILES['userfile']['name']; //get the file name
+			$tmpName = $_FILES['userfile']['tmp_name']; 
+			$fileSize = $_FILES['userfile']['size']; //get the size of the file
+			$fileType = $_FILES['userfile']['type']; //get the type of file
+			
+			//open the file and put the contents into '$content'
+			$fp = fopen($tmpName, 'r'); 
+			$content = fread($fp, filesize($tmpName));
+			$content = mysql_real_escape_string($content); 
+			fclose($fp); 
+			
+			$fileName = str_replace(' ', '', $fileName); //get rid of spaces
+			$fileName = pathinfo($fileName, PATHINFO_FILENAME); //get rid of the '.ext' at the end of the file
+			
+			$fileName = addslashes($fileName); //not sure what this does...
+			
+			//set what the query will do
+			$query = "INSERT INTO `assignmentfile`(`AssignmentID`, `UserID`, `FileID`, `FileName`, `FileData`) VALUES 
+			('1','Bob','1','$fileName','$content')";
+			
+			//call the query
+			MySQL::getInstance()->query($query) or die('<br>Error, file upload failed '. mysql_error());
+			echo "<br>File <i>$fileName</i> uploaded!</br>";
+		}
+	
+	} else { //file doesn't exist or not allowed extension
+		echo "in the else section <br></br>";
+		if($_FILES["userfile"]["error"] > 0) {
+			echo "Error: ". $_FILES["userfile"]["error"]. "<br>";
+		}
 	}
 ?>
