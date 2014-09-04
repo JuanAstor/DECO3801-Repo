024639f364ec5e83328db28ca3a5715ecfc4d87b
Naph <lachlandupreez@gmail.com>
2014-09-04 17:44:27 +1000

    Added MySQL Query: +get_assignments_to_mark()

--- lib/queries.php
+++ lib/queries.php
@@ -1,12 +1,16 @@
 <?php
-// Common SQL Query
 
-/*	EXAMPLE FUNCTION
+/**
+ * Returns the assignments assigned to the student for reviewing.
+ * Accesses table Reviewer and selects all FileID's where the
+ * UserID equals to the student ID.
  *
- *  function get_marks( $args ) {
- *		$query = MySQL::getInstance()->query("SELECT * FROM `Marks` WHERE `".$args."`");
- *		return $query->fetchALL();
- *	}
+ * @param 	$student : UserID of student to review
+ * @return 	List<String> : List of FileID's to mark.
  */
+function get_assigments_to_mark( $student ) {
+	$query = MySQL::getInstance()->query("SELECT * FROM `reviewer` WHERE `UserID`='$student'");
+	return $query->fetchALL();
+}
 
 ?>
