b79c363eb5b645385844ad8aad2283980ededb42
Steve Jacobson <steven.jacobson@uqconnect.edu.au>
2014-09-10 17:36:11 +1000

    added 'test_retrieval' file, gets a file from the database and when you click temp1.txt it displays the contents of the file from the database. Only supports retrieval of one file at the moment, need to change a few things for multiple file support

--- lib/retrieve_file.php
+++ lib/retrieve_file.php
@@ -1,14 +1,15 @@
 <?php
+session_start();
 require("mysql.php");
 require("queries.php");
-include("../view/codereview/header.php");
+//include("../view/codereview/header.php");
 
 
-function selectFile($id) {
-
+function selectFile($id, $file) {
+	//sql query to get the required file content
 	 $sql = "SELECT FileData
         FROM `assignmentfile`
-        WHERE UserID = 'Bob'"; //userId will need to match the vlue in the database (bob is not in the master branch)
+        WHERE UserID = '". $id ."' AND FileName = '". $file ."'"; 
  
     $stmt = MySQL::getInstance()->prepare($sql);
     $stmt->execute(array(":id" => $id));
@@ -19,9 +20,62 @@ function selectFile($id) {
 	
 	return array('FileData' => $data);  
  
+} 
+$arr = array(); //an array to store all the filenames(paths) that are created
+
+$a = selectFile('11112222', 'file1'); 
+echo $a['FileData']; //use $a['FileData'][0] to get the 1st file and so on if multiple files
+echo "<br>";
+ 
+//$fileName = "../mockup/files/tempfile.txt";
+$dir = "../mockup/files/";
+$fileName = "". $dir ."tempfile.txt";
+
+//check if the file already exists 
+
+while(check_if_file_exists($fileName) == 1){
+	//file name exists so loop until it doesn't
+	echo "file name exists, in loop";
+	$fileName =  "". $dir ."tempfile1.txt";
 }
 
-$a = selectFile('Bob'); //the parameter 'bob' is essentially useless, function selectFile isn't using $id
-echo $a['FileData'];
+	//add file names to an array 
+	array_push($arr, $fileName);
+	$_SESSION['array_name'] = $arr;
+	
+	$tmpFile = fopen($fileName, "w") or die("unable to create a temp file"); 
+	$txt = $a['FileData']; 
+	fwrite($tmpFile, $txt);
+	fclose($tmpFile); 
+	
+	echo "<br>";
+	echo readfile($fileName); 
+	echo "<br>";
+	
+	//delete the tempfile when finished with it
+	if(!unlink($fileName)){
+		//error, unable to delete file
+		echo "error deleting file";
+	}else {
+		//file has been deleted
+		echo "deleted";	
+	}
+//end of else 
 
+//checks the existance of a given file name
+function check_if_file_exists($fileName){
+	if(file_exists($fileName)){
+		//file does
+		return 1;
+	} else {
+		//file doesn't
+		return 0;
+	}
+}
+?>
+
+<?php 
+	$var = check_if_file_exists("file.txt");
+	echo "<br>";
+	echo "values are: ". $_SESSION['array_name'][0];
 ?>
