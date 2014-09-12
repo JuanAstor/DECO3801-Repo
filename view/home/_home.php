d2422c24aff95ec983389c63f78563311f3aa48a
Naph <lachlandupreez@gmail.com>
2014-09-12 22:05:19 +1000

    FUCK YES, HAHAHAHHAHAHAHAHAHHAHAHHAHAHAHAHAH YEEEEEEEEEEEEEEEEEEEEES. Dashboard Widgets Heights fixed

--- view/home/_home.php
+++ view/home/_home.php
@@ -1,37 +1,35 @@
 
-<sidebar>
-        <nav>
-        <p>Courses</p>
-                <?php // Query db for courses assigned to user ?>
-        </nav>
-</sidebar>
+<widget-container>
+    <widget title="Upcoming Assessments">
+        <panel>
+            <div title="heading"><i class="fa fa-clipboard"></i>Upcoming Assessments</div>
+            <div title="body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </panel>
+    </widget>
+    <!-- End Upcoming Assessments -->
+    <widget title="Tasks">
+        <panel>
+            <div title="heading"><i class="fa fa-file-code-o"></i>Tasks</div>
+            <div title="body">Panel content goes here..</div>
+        </panel>
+    </widget>
+    <!-- End Tasks -->
+    <widget title="Reviews Received">
+        <panel>
+            <div title="heading"><i class="fa fa-comment"></i>Reviews Received</div>
+            <div title="body">Panel content goes here..</div>
+        </panel>
+    </widget>
+    <!-- End Reviews Received -->
+</widget-container>
+<widget-end>
+    <div><panel-end></panel-end></div>
+    <div><panel-end></panel-end></div>
+    <div><panel-end></panel-end></div>
+</widget-end>
 
-<container>
-        <clearfix></clearfix>
-        <widget title="Upcoming Assessments">
-            <panel class="wtf">
-                <heading>Bootstrap 3 - Panel Primary</heading>
-                <div class="panel-body">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-            </panel>
-        </widget>
-        <!-- End Upcoming Assessments -->
-        <widget title="Tasks">
-            <panel class="wtf">
-                <heading>Bootstrap 3 - Panel Primary</heading>
-                <div class="panel-body">Panel content goes here..</div>
-            </panel>
-        </widget>
-        <!-- End Tasks -->
-        <widget title="Reviews Received">
-            <panel class="wtf">
-                <heading>Bootstrap 3 - Panel Primary</heading>
-                <div class="panel-body">Panel content goes here..</div>
-            </panel>
-        </widget>
-        <!-- End Reviews Received -->
+<div class="dashboard-content">
         <p> Welcome <?php echo $_SESSION["email"]; ?> </p>
-</container>
-
-<clearfix></clearfix>
+</div>
--- view/home/header2.php
+++ view/home/header2.php
@@ -2,16 +2,23 @@
 <html>
     <head>
     <title>Code Review</title>
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
         <!-- CSS/LESS -->
         <link rel="stylesheet/less" href="css/main.less">
         <!-- JS -->
         <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
         <script src='js/view.js'></script>
         <script src="js/less.js"></script>
-        <!-- Fonts -->
-        <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
     </head>
     <body>
     <header>
         <brand>Peer <span>{</span>Code Review<span>}</span></brand>
     </header>
+    <sidebar>
+        <nav>
+        <h5>Courses</h5>
+            <?php // Query db for courses assigned to user ?>
+        </nav>
+    </sidebar>
