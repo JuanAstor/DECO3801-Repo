69ac508dacbb6eecc6573079f7b70838af48d0f3
Naph <lachlandupreez@gmail.com>
2014-09-23 13:52:47 +1000

    Cleaned index.php

--- index.php
+++ index.php
@@ -7,47 +7,39 @@ if (!isset($_SESSION['user']) && isset($_POST['user'])) {
     $_SESSION['user'] = $_POST['user'];
 }
 
-?>
-<?php
-        /**
-         *  Show the home dashboard if student has authenticated.
-         *  Otherwise show the login prompt.
-         */
-        if (isset($_SESSION["user"]) && (get_login_status($_SESSION["user"]) == true)) 
-        {
-            $student = $_SESSION["user"];
-			//$result = get_login_status($student);
-			//echo $result; 
-			//check the login priveleges of the user 
-			if(check_if_admin($student) == 0){ 
-				
-				//user is a student, get info on who they are and what courses they do
-				$courses = get_users_courses($student);
-				$assessments = get_users_assessments($student);
-				$fullName = get_user_name($student);
-
-				// Show home dashboard
-    			include("view/home/header.php");
-				include("view/home/_home.php");
-			} 
-			else { 
-				//user is an admin
-				echo "this is admin time"; 
-				$fullName = get_user_name($student);
-				$courses = get_admin_courses($student); //will get the courses that an admin coordinates/tutes 	
-			}
-            
-        }
-        else
-        {
-			//remove any set userID session variable on the login screen
-			if(isset($_SESSION["user"])){
-				unset($_SESSION["user"]);	
-			}
-            // Show login prompt
-            include("view/login/_login.php"); 
-        }
+/**
+ *  Show the home dashboard if student has authenticated.
+ *  Otherwise show the login prompt.
+ */
+if (isset($_SESSION["user"]) && (get_login_status($_SESSION["user"]) == true)) {
     
-    //Footer
-    include("view/home/footer.php"); 
+    $user = $_SESSION["user"];
+    
+    if(!check_if_admin($user)){ 
+
+        // Student:
+        $courses = get_users_courses($user);
+        $assessments = get_users_assessments($user);
+        $fullName = get_user_name($user);
+
+        // Show home
+        include("view/home/header.php");
+        include("view/home/_home.php");
+        
+    }else{ 
+        
+        // Admin:
+        $fullName = get_user_name($user);
+        $courses = get_admins_courses($user);	
+    }
+}
+else{
+    session_destroy();
+    
+    // Show login
+    include("view/login/_login.php"); 
+}
+
+//Footer
+include("view/home/footer.php"); 
 ?>
--- lib/queries.php
+++ lib/queries.php
@@ -1,93 +1,75 @@
 <?php
 
-/**
- * Returns the assignments assigned to the student as string data
- * for reviewing. Using student ID, uses FileID's assigned to find 
- * FileData in table Assignmentfile and converts FileData blobs to
- * string. Returns as List of all FileData blob text as string.
- *
- * @param	$student : UserID of student to review
- * @return	List<String> : List of FileData as string to mark.
- * @author	Lachlan du Preez
- */
-function get_assigment_data_to_mark($student) {
-    $query = MySQL::getInstance()->query("SELECT CONVERT(`FileData` using utf8) 
-                                          FROM `reviewer`,`assignmentfile` 
-                                          WHERE reviewer.FileID = assignmentfile.FileID AND reviewer.UserID='.$student.'");
-    return $query->fetchALL();
-}
-
-function get_users_courses($student) {
+function get_users_courses($user) {
     $query = MySQL::getInstance()->query("SELECT CourseID
                                           FROM `courseenrolment` 
-                                          WHERE UserID = '" . $student . "'");
+                                          WHERE UserID = '" . $user . "'");
     return $query->fetchALL();
 }
 
-function get_admin_courses($id) {
+function get_admins_courses($user) {
     $query = MySQL::getInstance()->query("SELECT CourseID 
 					  FROM `course` 
-					  WHERE CourseCoordinator = '" . $id . "'");
+					  WHERE CourseCoordinator = '" . $user . "'");
     return $query->fetchALL();
 }
 
-function get_users_assessments($student) {
+function get_users_assessments($user) {
     $query = MySQL::getInstance()->query("SELECT * 
                                           FROM `assignment`,`courseenrolment`
-                                          WHERE courseenrolment.CourseID = assignment.CourseID AND courseenrolment.UserID='" . $student . "'");
+                                          WHERE courseenrolment.CourseID = assignment.CourseID AND courseenrolment.UserID='" . $user . "'");
     return $query->fetchAll();
 }
 
-function get_user_name($student) {
+function get_user_name($user) {
     $query = MySQL::getInstance()->query("SELECT * 
 					  FROM `user` 
-					  WHERE UserID = '" . $student . "'");
+					  WHERE UserID = '" . $user . "'");
     return $query->fetchALL();
 }
 
-function check_if_admin($id) { //returns 1 if an admin, 0 if student
+function check_if_admin($user) { //returns 1 if an admin, 0 if student
     $query = MySQL::getInstance()->query("SELECT Priveleges 
 					  FROM `user` 
-                                          WHERE UserID = '" . $id . "'");
+                                          WHERE UserID = '" . $user . "'");
     $result = $query->fetchALL();
     foreach ($result as $permission) {
         if (strcmp($permission['Priveleges'], "Admin") == 0) { //strcmp will return 0 if the strings are equal
-            return 1; //is an admin
-        } else {
-            return 0; //is a student
+            return true; //is an admin
         }
+        return false; //is a student
     }
 }
 
-function check_if_file_exists($userID, $assignmentID, $filename) {
+function check_if_file_exists($user, $assignmentID, $filename) {
     $query = MySQL::getInstance()->query("SELECT count(1)
                                           FROM `assignmentfile`
-					  WHERE UserID = '" . $userID . "' AND AssignmentID = '" . $assignmentID . "' AND FileName = '" . $filename . "'");
+					  WHERE UserID = '" . $user . "' AND AssignmentID = '" . $assignmentID . "' AND FileName = '" . $filename . "'");
     return $query->fetchALL(); //return the count of the number of files matching the variables
 }
 
-function update_file_contents($userID, $assignmentID, $filename, $content, $dateTime) {
-    $query = MySQL::getInstance()->query("UPDATE `assignmentfile`
-					  SET FileData = '" . $content . "', SubmissionTime = '" . $dateTime . "' 
-					  WHERE UserID = '" . $userID . "' AND AssignmentID = '" . $assignmentID . "' AND FileName = '" . $filename . "'");
+function update_file_contents($user, $assignmentID, $filename, $content, $dateTime) {
+    return MySQL::getInstance()->query("UPDATE `assignmentfile`
+					SET FileData = '" . $content . "', SubmissionTime = '" . $dateTime . "' 
+					WHERE UserID = '" . $user . "' AND AssignmentID = '" . $assignmentID . "' AND FileName = '" . $filename . "'");
 }
 
-function insert_file_data($userID, $assignmentID, $filename, $content, $dateTime) {
-    $query = MySQL::getInstance()->query("INSERT INTO `assignmentfile` (`AssignmentID`, `UserID`, `FileName`, `FileData`, `SubmissionTime`)
-                                          VALUES ('" . $assignmentID . "', '" . $userID . "', '" . $filename . "', '" . $content . "', '" . $dateTime . "')");
+function insert_file_data($user, $assignmentID, $filename, $content, $dateTime) {
+    return MySQL::getInstance()->query("INSERT INTO `assignmentfile` (`AssignmentID`, `UserID`, `FileName`, `FileData`, `SubmissionTime`)
+                                        VALUES ('" . $assignmentID . "', '" . $user . "', '" . $filename . "', '" . $content . "', '" . $dateTime . "')");
 }
 
-function get_files_to_comment($userID, $assignmentID) {
+function get_files_to_comment($user, $assignmentID) {
     $query = MySQL::getInstance()->query("SELECT * 
 					  FROM `assignmentfile` 
-					  WHERE UserID = '" . $userID . "' AND AssignmentID = '" . $assignmentID . "'");
+					  WHERE UserID = '" . $user . "' AND AssignmentID = '" . $assignmentID . "'");
     return $query->fetchALL();
 }
 
-function get_login_status($userID) {
+function get_login_status($user) {
     $query = MySQL::getInstance()->query("SELECT count(1)
 					  FROM `user`
-					  WHERE UserID = '" . $userID . "'");
+					  WHERE UserID = '" . $user . "'");
     $result = $query->fetchALL();
     $count = $result[0];
     if ($count[0] > 0) {
@@ -97,10 +79,10 @@ function get_login_status($userID) {
     }
 }
 
-function get_file_data($userID, $assignmentID, $filename){
-	$query = MySQL::getInstance()->query("SELECT *
+function get_file_data($user, $assignmentID, $filename){
+    $query = MySQL::getInstance()->query("SELECT *
                                           FROM `assignmentfile`
-                                          WHERE UserID = '".$userID."' AND AssignmentID = '".$assignmentID."' AND FileName = '".$filename."'"); 
+                                          WHERE UserID = '".$user."' AND AssignmentID = '".$assignmentID."' AND FileName = '".$filename."'"); 
     return $query->fetchALL();	
 }
 
