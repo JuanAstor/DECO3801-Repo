eafa9f017097b2ce0ba0701df4cc3909fa1eda13
Steve Jacobson <steven.jacobson@uqconnect.edu.au>
2014-09-28 19:49:58 +1000

    added files filename_text.php and upload1.php

--- mockup/upload_test.php
+++ mockup/upload_test.php
@@ -2,18 +2,30 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <title>Upload Test</title>
 </head>
 
+<script>
+                    jQuery(function ($) {
+                    $('.code').annotator();
+                    });        </script>
+
 <h1>Test File Upload</h1>
 <div>
-	<form method="post" action="../lib/upload.php" enctype="multipart/form-data">
+	
     <table>
     	<tr>
         	<td> Attach a File(s): You can select more than one </td> 
             <td>
             	<input type="hidden" name="MAX_FILE_SIZE" value="100000000" />
-                <input name="userfile[]" type="file" multiple /> <!-- Need to include the '[]' at the end of name! -->
+                <input name="userfile[]" id="ups" onchange="runThis()" type="file" multiple /> <!-- Need to include the '[]' at the end of name! -->					
+				<script>
+					function runThis(){
+						var file_name = $('input[type="file"]');
+						document.body.innerHTML = file_name;	
+					}
+				</script>
             </td>
         </tr>
         
@@ -23,13 +35,53 @@
             	</p>
             </td>
             <td>
-            	<input type="submit" class="box" value="Send"  />
+            	<input type="submit" class="aSubmit" value="Send"  />
                 <input type="reset" value="Reset"  />
             </td>
         </tr>
     </table> 
-    </form>
+    
+     <div class="code">
+            <pre class="prettyprint"> </pre>
+        </div>
 </div>
 
-</body>
+</body> 
+
+<script>
+
+jQuery(function ($) {
+	
+
+    $("#aForm").submit(function(event) {
+    var files = $('input[type="file"]').val();
+        // MAKE SURE THAT ASSIGNID AND USERID IS ALWAYS VALID.
+        $.ajax({
+            type:'POST',
+            url:'../lib/upload1.php',
+            data: {filename : files,
+                   user : '<?php echo $uID ?>',
+                   assign : '<?php echo $assignID ?>' },
+			async: false,
+			contentType: false,
+			processData: false,
+            success: function(data){
+                $("pre").text(data);
+                
+            }
+        });
+		event.preventDefault();
+    });
+	
+	$(".aSubmit").click(function()){
+		$("#aForm").submit();
+	});
+	
+    $('#code').annotator();
+
+});
+                    
+</script>
+
+
 </html>
