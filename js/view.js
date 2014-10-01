6fe9a227b7c2d08c10c36d20dafe220bd0ec2c16
Naph <lachlandupreez@gmail.com>
2014-10-01 15:13:01 +1000

    Fixed that annoying scrolling issue

--- js/view.js
+++ js/view.js
@@ -1,15 +1,21 @@
 // JavaScript Document
 $(document).ready(function(){
-     
-        $('nav h4:first').click(function(){
-            window.location.href = "index.php";
-        });
-        
-	$('nav h4').click(function () {
-		$(this).next('navgroup').stop().slideToggle(400);
-	});
+    
+    $('page').css("min-height", $(window).height() - 50 );
+    
+    $('nav h4:first').click(function(){
+        window.location.href = "index.php";
+    });
 
-	$('nav-handle').click(function () {
-		$('nav').stop().slideToggle(400);
-	});
+    $('nav h4').click(function () {
+            $(this).next('navgroup').stop().slideToggle(400);
+    });
+
+    $('nav-handle').click(function () {
+            $('nav').stop().slideToggle(400);
+    });
+});
+
+$(window).resize(function() {
+    $('page').css("min-height", $(window).height() - 50 ); 
 });
