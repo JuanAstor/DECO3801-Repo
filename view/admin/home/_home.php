1c6c1ac660c952522d9de110dc73b5412b07b4ab
Steve Jacobson <steven.jacobson@uqconnect.edu.au>
2014-10-21 12:59:11 +1000

    Updated admin home page
    removed buttons, added anchors instead

--- view/admin/home/_home.php
+++ view/admin/home/_home.php
@@ -23,9 +23,8 @@
 							echo "<br />"; 
 							$result = get_course_assessments($course['CourseID'], $course['Semester']);
 							foreach($result as $name){	
-								//echo "<span>--------------</span>";
-								echo $name['AssignmentName'];
-								echo "<a href='/EditAssessment.php?course=".$course['CourseID']."&sem=".$course['Semester']."&name=".$name['AssignmentName']."'><button type='button' class='btn btn-primary'>Go</button></a>";
+								//display the assignment name as a link
+								echo "<a href='/EditAssessment.php?course=".$course['CourseID']."&sem=".$course['Semester']."&name=".$name['AssignmentName']."'>".$name['AssignmentName']."</a>";
 								echo "<br />";
 							}
 							echo "<br />";
@@ -43,8 +42,10 @@
 						foreach($courses as $course){
 							//display all courses that the admin is in charge of
 							echo "<span>".strtoupper($course['CourseID'])."   |  </span>";
-							echo "<span>Sem ".substr($course['Semester'], -1)."    ".substr($course['Semester'],0,4)."      </span>";
-							echo "<a href='/StudentReviews.php?course=".$course['CourseID']."&sem=".$course['Semester']."'><button type='button' class='btn btn-primary'>Go</button></a>";
+							echo "<span>Semester ".substr($course['Semester'], -1)."    ".substr($course['Semester'],0,4)."      </span><br />";
+							echo "<a href='/StudentReviews.php?course=".$course['CourseID']."&sem=".$course['Semester']."'>
+							Review
+							</a>";
 							echo "<br /> <br />";
 						}
 					?>
